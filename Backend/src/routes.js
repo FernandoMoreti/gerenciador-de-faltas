0ae08d11c5cc8227c619c7dd8e23378446a5dc0a
Fernando Moreti Bolela e Silva <153198754+FernandoMoreti@users.noreply.github.com>
2025-07-22 22:04:48 -0300

    Organizando o Routes
    Organizando o routes

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -13,96 +13,70 @@ const router = express.Router();
 router.get("/salas", SalasController.index)
 router.get("/salas/:id", SalasController.showSalas)
 router.get("/salasAlunos/:id", SalasController.showAlunos)
-
-// Post Salas
-router.post("/salas/create", SalasController.create)
-
-// Put Salas
-router.put("/salas/update/:id", SalasController.update)
-
-// Delete salas
-router.delete("/salas/:id", SalasController.delete)
-
-
 // Get Alunos
 router.get("/Aluno", AlunoController.index)
 router.get("/Aluno/:id", AlunoController.show)
-
-// Post Aluno
-router.post("/Aluno/create", AlunoController.create)
-
-// Put Aluno
-router.put("/Aluno/update/:id", AlunoController.update)
-
-// Delete Aluno
-router.delete("/Aluno/:id", AlunoController.delete)
-
-
 // Get Professor
 router.get("/professor", ProfController.index)
-
-// Get Professor Login
-router.post("/professor/login", ProfController.findByCod)
-
-// Post Professor
-router.post("/professor/create", ProfController.create)
-
-// Put Professor
-router.put("/professor/update/:id", ProfController.update)
-
-// Delete Professor
-router.delete("/professor/:id", ProfController.delete)
-
-
 // Get Escola 
 router.get("/escola", EscolaController.index)
 router.get("/escola/sala/:id", EscolaController.showEscolaBySala)
-
-// Post Escola
-router.post("/escola/create", EscolaController.create)
-
-// Put Escola
-router.put("/Escola/update/:id", EscolaController.update)
-
-// Delete Escola
-router.delete("/escola/:id", EscolaController.delete)
-
-
+// Get ProfEscola
+router.get("/ProfEscola", ProfEscolaController.index)
+router.get("/profEscola/:id", ProfEscolaController.showEscolasByProf)
+// Get Falta
+router.get("/Falta", FaltaController.index)
+// Get Falta Aluno
+router.get("/Falta/:id", FaltaController.showByAluno)
 // Get Aula
 router.get("/Aula", AulaController.index)
 router.get("/Aula/:id", AulaController.show)
 
+
+// Post Salas
+router.post("/salas/create", SalasController.create)
+// Post Aluno
+router.post("/Aluno/create", AlunoController.create)
+// Get Professor Login
+router.post("/professor/login", ProfController.findByCod)
+// Post Professor
+router.post("/professor/create", ProfController.create)
+// Post Escola
+router.post("/escola/create", EscolaController.create)
 // Post Aula
 router.post("/Aula/create", AulaController.create)
-
-// Put Aula
-router.put("/Aula/update/:id", AulaController.update)
-
-// Delete Aula
-router.delete("/Aula/:id", AulaController.delete)
-
-
-// Get ProfEscola
-router.get("/ProfEscola", ProfEscolaController.index)
-router.get("/profEscola/:id", ProfEscolaController.showEscolasByProf)
-
 // Post ProfEscola
 router.post("/ProfEscola/create", ProfEscolaController.create)
-
-// Delete ProfEscola
-router.delete("/ProfEscola/:id", ProfEscolaController.delete)
-
-
-// Get Falta
-router.get("/Falta", FaltaController.index)
-
-// Get Falta Aluno
-router.get("/Falta/:id", FaltaController.showByAluno)
-
 // Post Falta
 router.post("/Falta/create", FaltaController.create)
 
+
+// Put Salas
+router.put("/salas/update/:id", SalasController.update)
+// Put Aluno
+router.put("/Aluno/update/:id", AlunoController.update)
+// Put Professor
+router.put("/professor/update/:id", ProfController.update)
+// Put Escola
+router.put("/Escola/update/:id", EscolaController.update)
+// Put Aula
+router.put("/Aula/update/:id", AulaController.update)
+
+
+// Delete salas
+router.delete("/salas/:id", SalasController.delete)
+// Delete Aluno
+router.delete("/Aluno/:id", AlunoController.delete)
+// Delete Professor
+router.delete("/professor/:id", ProfController.delete)
+// Delete Escola
+router.delete("/escola/:id", EscolaController.delete)
+// Delete Aula
+router.delete("/Aula/:id", AulaController.delete)
+// Delete ProfEscola
+router.delete("/ProfEscola/:id", ProfEscolaController.delete)
 // Delete Falta
 router.delete("/Falta/:id", FaltaController.delete)
+
 
 module.exports = router;
